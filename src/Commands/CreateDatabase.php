a5b10e9e3b072ae5bae71e655a3fc0c305d57bd1
Dmitry Kozachkov <d.kozachkov@stamax.com.ua>
2018-02-26 10:40:03 +0200

    Refactor

--- src/Commands/CreateDatabase.php
+++ src/Commands/CreateDatabase.php
@@ -3,6 +3,8 @@
 namespace Dmitrykozachkov\LaravelCreateDatabase\Commands;
 
 use Illuminate\Console\Command;
+use \PDO;
+use \Artisan;
 
 class CreateDatabase extends Command
 {
@@ -14,7 +16,7 @@ class CreateDatabase extends Command
      *
      * @var string
      */
-    protected $signature = 'db:create';
+    protected $signature = 'db:create {--migrate} {--seed}';
 
     /**
      * The console command description.
@@ -40,15 +42,35 @@ class CreateDatabase extends Command
      */
     public function handle()
     {
+        $connection = env('DB_CONNECTION');
         $host = env('DB_HOST');
+        $port = env('DB_PORT');
         $database = env('DB_DATABASE');
         $user_name = env('DB_USERNAME');
         $password = env('DB_PASSWORD');
 
-        $pdo = new \PDO("mysql:host=$host", $user_name, $password);
+        $pdo = new PDO("$connection:host=$host;port=$port", $user_name, $password);
 
         $sql = "CREATE DATABASE " . env('DB_DATABASE') . " CHARACTER SET utf8 COLLATE utf8_general_ci";
+
         $stmt = $pdo->prepare($sql);
-        $stmt->execute() ? $this->line('Database was created') : $this->error('ERROR: unable to create database');
+
+        if ($stmt->execute()) {
+            $this->line('Database was created.');
+        } else {
+            $this->error('ERROR: unable to create database!');
+            die;
+        }
+
+        if ($this->option('migrate')) {
+            Artisan::call('migrate');
+            $this->line('Migration table created successfully.');
+        }
+
+        if ($this->option('seed')) {
+            Artisan::call('db:seed');
+        }
+
+        $this->info('Done.');
     }
 }
